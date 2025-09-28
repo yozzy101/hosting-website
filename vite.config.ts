d7bead5270d5452190887873f58c88626677e440
yozzy101 <132937497+yozzy101@users.noreply.github.com>
2025-09-28 01:20:56 -0500

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [react()],
+})
